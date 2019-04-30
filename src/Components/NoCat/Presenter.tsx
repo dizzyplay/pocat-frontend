c6fbb77b39572ed6f1c93c41e45148fab041ec79
vovo <vovoboss@gmail.com>
2019-04-30 15:33:42 +0900

    no cat

---
+++ src/Components/NoCat/Presenter.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from "styled-components";
+import { mytheme } from "../../Styles/Theme";
+import { Button } from "../Button";
+
+export default () => {
+  // @ts-ignore
+  return (
+    <Wrapper>
+      <Container>
+        <Title size={"32px"} weight={"bold"}>
+          야옹이의 정보를 입력해주세요
+        </Title>
+        <Title size={"16px"}>
+          건강한 반려묘를 위해 체중관리는 필수
+          <br />
+          행복한 야옹이를 위해 포캣에서 건강을 관리하세요
+        </Title>
+        <div style={{ width: "149px", marginTop: "30px" }}>
+          <Button title={"정보입력하기"} primary={true} />
+        </div>
+      </Container>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 800px;
+`;
+
+const Container = styled.div`
+  width: 450px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  margin-top: 100px;
+`;
+
+interface TType {
+  size: string;
+  weight?: string;
+  theme: {
+    [key: string]: any;
+  };
+}
+const Title = styled.div`
+  color: ${(props: mytheme) => props.theme.blue};
+  font-size: ${(props: TType) => props.size};
+  font-weight: ${(props: TType) => props.weight};
+  line-height: 1.19;
+  letter-spacing: -0.6px;
+  padding-bottom: 15px;
+`;
