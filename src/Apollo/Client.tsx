c94f20cf19052f9931e6970a1af7d7d84f7332e9
dizzyplay <vovoboss@gmail.com>
2019-06-11 18:42:01 +0900

    add env

--- src/Apollo/Client.tsx
+++ src/Apollo/Client.tsx
@@ -1,17 +1,17 @@
-import ApolloClient, { gql } from "apollo-boost";
-import { defaults, resolvers } from "./LocalState";
-import { InMemoryCache } from "apollo-cache-inmemory";
+import ApolloClient, {gql} from 'apollo-boost';
+import {defaults, resolvers} from './LocalState';
+import {InMemoryCache} from 'apollo-cache-inmemory';
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  uri: "http://10.0.1.19:3006",
+  uri: process.env.REACT_APP_API_URL || 'http://10.0.1.19:3006',
   clientState: {
     defaults,
-    resolvers
+    resolvers,
   },
   headers: {
-    authorization: `Bearer ${localStorage.getItem("token")}`
-  }
+    authorization: `Bearer ${localStorage.getItem('token')}`,
+  },
 });
 
 export default client;
